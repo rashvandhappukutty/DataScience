77f622bdf13f4c04a98fbddd6fbbe72e42d4e098
rtrrashvandh-tech <rtrrashvandh@gmail.com>
2026-01-03 11:56:11 +0530

    first commit

---
+++ Day 10/readOperationwith writeoperation.py
@@ -0,0 +1,6 @@
+with open('Day 10/file.txt','r') as f:
+    print(f.read())
+with open('Day 10/file.txt','r') as f:
+    print(f.readline())
+with open('Day 10/file.txt','r') as f:
+    print(f.readlines())
